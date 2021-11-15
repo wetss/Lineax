f41543f71662e7355ef65413f94bba23ebea9ce9
wetss <83474873+wetss@users.noreply.github.com>
2021-11-15 10:40:41 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer class="barra-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 pt-4 ">
+                <h3 class="titulosige">Síguenos en:</h3>
+                <div class="icon1s">
+                    <a class="icon1" href="https://lineaxrl.blogspot.com/"><img src="img/blogger.png" width="38" alt="" ></a>
+                    <a class="icon1" href="https://sites.google.com/view/lineax/inicio"><img src="img/google.png" alt="" ></a>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 pt-4 ">
+                <div class="container text-center pr-5 mb-4">
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 pt-4">
+                <div class="container mb-4">
+                    <h3 class="text-white text-center font-weight-bold">CONTACTO</h3>
+                </div>
+                <div class="text-white">
+                    <i class="fa fa-map-marker m-2" aria-hidden="true"></i><span>Lima-Peru</span> <br>
+                    <i class="fa fa-envelope m-2" aria-hidden="true"></i> <span><a class="text-white " href="#">asdasda@gmail.com</a></span><br>
+                    <i class="fa fa-phone m-2" aria-hidden="true"></i>
+                    <span><a class="text-white" href="#">123456789</a> 
+                    </span>
+                    <br>
+                </div>
+                <div class="iconall row d-flex justify-content-right mx-1">
+                    <ul class="social-icons margin-top-10 nav">
+                        <li class="nav-item facebook4">
+                            <a class="nav-link" href="#" target="black"><i class="text-white fab fa-facebook-f"></i></a>
+                        </li>
+                        <li class="nav-item google-plus4 iconoYoutube">
+                            <a class="nav-link" href="#" target="black"><i class="text-white fab fa-youtube"></i></a>
+                        </li>
+                        <li class="nav-item instagram4">
+                            <a class="nav-link" href="#" target="black"><i class="text-white fab fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="container-fluid justify-content-center">
+                    <p class="text-white text-center">®Copyright © 2021 Lineax | All Rights Reserved<p>
+            </div>
+        </div>
+    </div>
+</footer>
