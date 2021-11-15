1443e05ddcfd61d8af8c27e9e333a3515c093505
wetss <83474873+wetss@users.noreply.github.com>
2021-11-15 10:44:10 -0500

    Create servicios.php

---
+++ servicios.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="fonts.css">
+    <script src="https://kit.fontawesome.com/0458944bda.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Prueba</title>
+</head>
+
+<body>
+<header class="header">
+        <div class="container logo-nav-container">
+            <a href="#"><img src="img/logo15.png" class="logo1" alt=""></a>
+            <span class="menu-icon">MENU</span>
+        
+            <nav class="navigation">
+                <ul class="show">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="acerca.php">Acerca</a></li>
+                    <li><a href="servicios.php">Servicios</a></li>
+                    <li><a href="contactenos.php">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+<main>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div>
+        <h1>Envios de productos</h1>
+    </div>
+
+</main>
+</body>
+</html>
