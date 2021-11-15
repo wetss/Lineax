db8e14a5d3695ccca00d8b9f38422b6e1d1e577d
wetss <83474873+wetss@users.noreply.github.com>
2021-11-15 10:41:02 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Ropa 2021, Lineax">
+    <meta name="robots" content="index, follow">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="fonts.css">
+    <script src="https://kit.fontawesome.com/0458944bda.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Lineax</title>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XHYBC3C8MK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XHYBC3C8MK');
+</script>
+
+</head>
+
+<body>
+    <header class="header">
+        <div class="container logo-nav-container">
+            <a href="#"><img src="img/logo15.png" class="logo1" alt=""></a>
+            <span class="menu-icon">MENU</span>
+        
+            <nav class="navigation">
+                <ul class="show">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="acerca.php">Acerca</a></li>
+                    <li><a href="servicios.php">Servicios</a></li>
+                    <li><a href="contactenos.php">Contacto</a></li>
+                    <li><a class="btn btn-outline-light btn-sm" href="iniciar.php" style="color:white;">INICIAR SESIÓN</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="main">
+    <div class="carouseltotal">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="imagen d-block w-100" src="img/m1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="imagen d-block w-100" src="img/m2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="imagen d-block w-100" src="img/m3.jpg" alt="Third slide">
+                </div>
+            </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                 </a>
+          </div>
+        </div>
+        <div class="containereloj">
+                <div class="tituloreloj">
+                    <h1 class="text-white">Sección de relojes</h1>
+                </div>
+                <div class="card1" style="width: 15rem;">
+                    <img class="card-img-top" src="img/reloj1.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Tag Heuer</h5>
+                      <p class="card-text">Fundada por Edouard Heuer en 1860 esta marca se ha especializado en relojes con cronógrafo.</p>
+                      <a href="#" class="btn btn-primary">Adquirir</a>
+                    </div>
+                </div>
+                <div class="card1" style="width: 15rem;">
+                    <img class="card-img-top" src="img/reloj2.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Rolex</h5>
+                      <p class="card-text">Empezamos por un clásico. Cuando cualquier persona piensa en relojes de lujo la marca que siempre se nos viene a la mente es Rolex</p>
+                      <a href="#" class="btn btn-primary">Adquirir
+                      </a>
+                    </div>
+                </div>
+                <div class="card1" style="width: 15rem;">
+                    <img class="card-img-top" src="img/reloj3.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Tag Heuer</h5>
+                      <p class="card-text">Fundada por Edouard Heuer en 1860 esta marca se ha especializado en relojes con cronógrafo.</p>
+                      <a href="#" class="btn btn-primary">Adquirir</a>
+                    </div>
+                </div>
+                <div class="card1" style="width: 15rem;">
+                    <img class="card-img-top" src="img/reloj4.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Reloj Omega</h5>
+                      <p class="card-text">Omega se fundó en 1848 por Louis Brandt, y esta marca está llena de hitos históricos que la han convertido en un icono en el mundo de la relojería</p>
+                      <a href="#" class="btn btn-primary">Adquirir</a>
+                    </div>
+                </div>
+        </div>
+        <br>
+                <div class="containerpo">
+                    <div class="tituloreloj">
+                        <h2 class="text-white">Sección Polos Deportivos</h2>
+                    </div>
+                    <div class="card1" style="width: 15rem;">
+                        <img class="card-img-top" src="img/polos5.jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Polo Premium</h5>
+                        <p class="card-text">Polo Premium 100% algodón peruano - Negro</p>
+                        <a href="#" class="btn btn-primary">Adquirir</a>
+                        </div>
+                    </div>
+                    <div class="card1" style="width: 15rem;">
+                        <img class="card-img-top" src="img/polos3.jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Polo Adidas</h5>
+                        <p class="card-text">Polo Adidas Hombre M Lin Sj T Negro</p>
+                        <a href="#" class="btn btn-primary">Adquirir</a>
+                        </div>
+                    </div>
+                    <div class="card1" style="width: 15rem;">
+                        <img class="card-img-top" src="img/polos2.jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Polo Umbro</h5>
+                        <p class="card-text">Polo Umbro Hombre Dr20040-Wht T-Shirt Blanco</p>
+                        <a href="#" class="btn btn-primary">Adquirir</a>
+                        </div>
+                    </div>
+                    <div class="card1" style="width: 15rem;">
+                        <img class="card-img-top" src="img/polos4.jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Polo Umbro</h5>
+                        <p class="card-text">Polo Umbro Hombre T-Shirt Estampado Dr20029-Bsm</p>
+                        <a href="#" class="btn btn-primary">Adquirir</a>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="containerpo">
+                    <div class="tituloreloj">
+                        <h2 class="text-white">Sección Pantalones</h2>
+                    </div>
+                    <div class="card1" style="width: 15rem;">
+                        <img class="card-img-top" src="img/pan.jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Pantalones Buzo Umbro</h5>
+                        <p class="card-text">Pantalón de Buzo Umbro Hombre Slim Fit Fleece Jogger 65677U-263 Gris Claro</p>
+                        <a href="#" class="btn btn-primary">Adquirir</a>
+                        </div>
+                    </div>
+                    <div class="card1" style="width: 15rem;">
+                        <img class="card-img-top" src="img/pan.jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Pantalones Podium</h5>
+                        <p class="card-text">Pantalón de Buzo Podium Hombre Pantalon Mesa Gris</p>
+                        <a href="#" class="btn btn-primary">Adquirir</a>
+                        </div>
+                    </div>
+                    <div class="card1" style="width: 15rem;">
+                        <img class="card-img-top" src="img/pan.jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Pantalones Puma</h5>
+                        <p class="card-text">Pantalón de Buzo Puma Hombre Logo Sweat P Gris</p>
+                        <a href="#" class="btn btn-primary">Adquirir</a>
+                        </div>
+                    </div>
+                    <div class="card1" style="width: 15rem;">
+                        <img class="card-img-top" src="img/pan.jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Pantalones de Buzo Umbro </h5>
+                        <p class="card-text">Pantalón de Buzo Umbro Hombre 55211U-Kd9 Pg Graphic Gris Pearl</p>
+                        <a href="#" class="btn btn-primary">Adquirir</a>
+                        </div>
+                    </div>
+                </div>
+    </main>
+    <a class="gotopbtn" href="#"><i class="fas fa-arrow-up"></i></a>
+    <?php
+        include'footer.php';
+    ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
+    
