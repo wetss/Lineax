bd327acc88a04791ccfd1883b541d827ea270ae1
wetss <83474873+wetss@users.noreply.github.com>
2021-11-15 10:39:37 -0500

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,11 @@
+
+<?php
+try {
+
+    $db = new PDO('mysql:host=localhost;
+    dbname=id17589235_lineax2','id17589235_root','&8Yuuv2z@6laz/L*');
+} catch (Exception $e) {
+
+    die($e->getMessage());
+}
+?>
