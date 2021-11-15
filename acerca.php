0e3db8f586f4d005a0f411bc5aebf9034733f371
wetss <83474873+wetss@users.noreply.github.com>
2021-11-15 10:38:23 -0500

    Create acerca.php

---
+++ acerca.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="fonts.css">
+    <script src="https://kit.fontawesome.com/0458944bda.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Acerca</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="container logo-nav-container">
+            <a href="#"><img src="img/logo15.png" class="logo1" alt=""></a>
+            <span class="menu-icon">MENU</span>
+        
+            <nav class="navigation">
+                <ul class="show">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="acerca.php">Acerca</a></li>
+                    <li><a href="servicios.php">Servicios</a></li>
+                    <li><a href="contactenos.php">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="main">
+        <div class="cajacerca">
+            <h1 class="text-center mt-2">Misión</h1>
+            <p>Ofrecer a las damas que gustan del buen vestir, ropa de la más alta calidad al mejor precio del mercado, 
+            siempre tratando de tener modelaje que este al corriente con lo último de la moda, para que así pueda desarrolar sus actividad cotidianas luciendo su belleza y resaltando su elegancia.</p>
+            <img src="img/no.jpeg" class="ima1" width="500" alt="">
+        </div>
+        <div class="cajacerca">
+            <h1 class="text-center mt-3">Visión</h1>
+            <p>Posicionarnos en el mercado como una empresa exitosa y socialmente responsable con gran reconocimiento a nivel nacional gracias a nuestros productos,
+            calidad, precios y servicio, siempre respetando los derechos y el trabajo tanto de nuestros colaboradores como de nuestros clientes, para asi seguir creciendo y poder ofrecer nuestros productos en todo el interior del país.</p>
+            <img src="img/no2.jpeg" class="ima1" width="500" alt="">
+
+        </div>
+        <div class="cajacerca">
+            <h1 class="text-center mt-3">Objetivos</h1>
+            <p>Ofrecer ropa de mejor calidad satisfaga las  necesidades y gustos del mercado potencial, 
+            vender ropa a la moda que cumpla con los requerimientos de estilo en el segmento  del mercado.</p>
+            <img src="img/no3.jpeg" class="ima1" width="500" alt="">
+                
+        </div>
+    </main>
+    <a class="gotopbtn" href="#"><i class="fas fa-arrow-up"></i></a>
+    <?php
+        include'footer.php';
+    ?>
+</body>
+</html>
